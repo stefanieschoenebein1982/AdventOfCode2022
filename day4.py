6863ae54f26fdc80ed8b8c4efefeb4c449099d0d
Schoenebein, Stefanie <stefanie.schoenebein@otto.de>
2023-01-15 21:46:21 +0100

    convert string to int before comparing numbers in day4 challenge

--- day4.py
+++ day4.py
@@ -5,22 +5,11 @@ with open('day4.txt') as f:
 
 # task 1
 def completeOverlapExistsFor(firstElveMin, firstElveMax, secondElveMin, secondElveMax):
-    print('Input: ' + str(firstElveMin) + ' ' + str(firstElveMax) + ' ' + str(secondElveMin) + ' ' + str(secondElveMax))
     if firstElveMin <= secondElveMin and firstElveMax >= secondElveMax:
-        print('firstCase')
-        print(firstElveMin + ' <= ' + secondElveMin + ' = true')
-        print(firstElveMax + ' >= ' + secondElveMax + ' = true')
-        print(' ')
         return True
     elif secondElveMin <= firstElveMin and secondElveMax >= firstElveMax:
-        print('secondCase')
-        print(secondElveMin + ' <= ' + firstElveMin + ' = true')
-        print(secondElveMax + ' >= ' + firstElveMax + ' = true')
-        print(' ')
         return True
     else:
-        print('False')
-        print(' ')
         return False
 
 foundCompleteOverlapsCounter = 0
@@ -28,10 +17,10 @@ for line in contents:
     bothTasks = line.split(',')
     firstElveTasks = bothTasks[0].split('-')
     secondElveTasks = bothTasks[1].split('-')
-    firstElveMin = firstElveTasks[0].strip()
-    firstElveMax = firstElveTasks[1].strip()
-    secondElveMin = secondElveTasks[0].strip()
-    secondElveMax = secondElveTasks[1].strip()
+    firstElveMin = int(firstElveTasks[0].strip())
+    firstElveMax = int(firstElveTasks[1].strip())
+    secondElveMin = int(secondElveTasks[0].strip())
+    secondElveMax = int(secondElveTasks[1].strip())
 
     if completeOverlapExistsFor(firstElveMin, firstElveMax, secondElveMin, secondElveMax):
         foundCompleteOverlapsCounter += 1
